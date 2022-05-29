f382bc77610277e481e066136b99e9eeb4ebeb1f
Fernando <luisgutierrez5-1@dgb.email>
2022-05-29 14:18:56 -0500

    init commit

--- app/src/main/java/mx/ita/ayudadita/MainActivity.java
+++ app/src/main/java/mx/ita/ayudadita/MainActivity.java
@@ -29,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.SplashTheme);
-        super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Ayudadita);
+        super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
