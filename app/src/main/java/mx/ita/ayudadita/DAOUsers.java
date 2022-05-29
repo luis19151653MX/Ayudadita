9bc56fc32c1e9b18513560084e0d653d250ca359
Fernando <luisgutierrez5-1@dgb.email>
2022-05-29 16:53:38 -0500

    Revert "Added"
    This reverts commit c300e283

--- app/src/main/java/mx/ita/ayudadita/DAOUsers.java
+++ app/src/main/java/mx/ita/ayudadita/DAOUsers.java
@@ -1,19 +1,4 @@
 package mx.ita.ayudadita;
 
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class DAOUsers {
-    private DatabaseReference databaseReference;
-
-    public DAOUsers(){
-        FirebaseDatabase db = FirebaseDatabase.getInstance("hhttps://projectdmayudaita-default-rtdb.firebaseio.com/");
-        databaseReference=db.getReference(Users.class.getSimpleName());
-    }
-
-    /*Insert*/
-    public Task<Void> add(Users user){
-        return databaseReference.push().setValue(user);
-    }
 }
