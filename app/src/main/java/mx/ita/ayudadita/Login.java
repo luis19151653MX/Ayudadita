fbbdd488655b922fa4b60b4dabbfb1bead84d2b4
CassandraHZ <106196257+CassandraHZ@users.noreply.github.com>
2022-05-30 15:48:48 -0500

    Added registrar y login

--- app/src/main/java/mx/ita/ayudadita/Login.java
+++ app/src/main/java/mx/ita/ayudadita/Login.java
@@ -1,26 +1,82 @@
 package mx.ita.ayudadita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import mx.ita.ayudadita.ui.home.HomeFragment;
 
 public class Login extends AppCompatActivity {
+    private EditText edCorreo;
+    private EditText edContraseña;
+    private Button Login;
+    private Button Registrar;
+
+    private String correo;
+    private String contraseña;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button crear_cuenta = findViewById(R.id.crear_cuenta);
-            crear_cuenta.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(view.getContext(), Registration.class);
-                    startActivityForResult(intent,0);
+        mAuth= FirebaseAuth.getInstance();
+
+        edCorreo = (EditText) findViewById(R.id.email);
+        edContraseña = (EditText) findViewById(R.id.password);
+        Registrar = (Button) findViewById(R.id.crear_cuenta);
+        Login= (Button) findViewById(R.id.login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                correo = edCorreo.getText().toString();
+                contraseña = edContraseña.getText().toString();
+
+                if(!correo.isEmpty() && !contraseña.isEmpty()){
+                    loginUser();
+                }else{
+                    Toast.makeText(Login.this, "Completa los campos", Toast.LENGTH_SHORT).show();
                 }
-            });
+            }
+        });
+
+        Registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, HomeFragment.class));
+            }
+        });
+
+    }
+
+    private void loginUser(){
+        mAuth.signInWithEmailAndPassword(correo, contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(Login.this, HomeFragment.class));
+                }else{
+                    Toast.makeText(Login.this, "No pudo iniciar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/mx/ita/ayudadita/Registration.java
+++ app/src/main/java/mx/ita/ayudadita/Registration.java
@@ -1,38 +1,109 @@
 package mx.ita.ayudadita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Registration extends AppCompatActivity {
+
+    private Button Registrar;
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
+    private String nombre;
+    private String correo;
+    private String contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        final EditText nombre = findViewById(R.id.nombre);
-        final EditText correo = findViewById(R.id.correo);
-        final EditText contraseña = findViewById(R.id.contraseña);
 
-        Button btn = findViewById(R.id.registrar);
-        DAOUsers dao = new DAOUsers();
+        mAuth= FirebaseAuth.getInstance();
+        mDatabase= FirebaseDatabase.getInstance().getReference();
+        final EditText edit_nombre = findViewById(R.id.edit_nombre);
+        final EditText edit_correo = findViewById(R.id.edit_correo);
+        final EditText edit_contraseña = findViewById(R.id.edit_contraseña);
+        Registrar = (Button) findViewById(R.id.registrar);
 
-        btn.setOnClickListener(v->
-        {
-            /*INSERTAR*/
-            Users user = new Users(nombre.getText().toString(), correo.getText().toString(), contraseña.getText().toString());
-            dao.add(user).addOnSuccessListener(suc->
-            {
-                Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show();
-            }).addOnFailureListener(er->
-            {
-                Toast.makeText(this, ""+er.getMessage(), Toast.LENGTH_SHORT).show();
-            });
+        Registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombre = edit_nombre.getText().toString();
+                correo = edit_correo.getText().toString();
+                contraseña = edit_contraseña.getText().toString();
+
+                if (!nombre.isEmpty() && !correo.isEmpty() && !contraseña.isEmpty()){
+                    if (contraseña.length()>=6){
+                        registerUser();
+                    }else{
+                        Toast.makeText(Registration.this, "Debe de ternar +6",Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(Registration.this, "Completa los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void registerUser(){
+        mAuth.createUserWithEmailAndPassword(correo, contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("nombre", nombre);
+                    map.put("correo", correo);
+                    map.put("contraseña", contraseña);
+
+                    String id= mAuth.getCurrentUser().getUid();
+
+                    mDatabase.child("Usuarios").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if (task2.isSuccessful()){
+                                startActivity(new Intent(Registration.this, Login.class));
+                                finish();
+                            }else{
+                                Toast.makeText(Registration.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(Registration.this, "No se pudo registrar", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
 
 }
