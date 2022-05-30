58f9dd0632b2f044900aa887f016780f2e5953ed
CassandraHZ <106196257+CassandraHZ@users.noreply.github.com>
2022-05-29 19:21:34 -0500

    Added

--- app/src/main/java/mx/ita/ayudadita/Login.java
+++ app/src/main/java/mx/ita/ayudadita/Login.java
@@ -2,7 +2,10 @@ package mx.ita.ayudadita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button crear_cuenta = findViewById(R.id.crear_cuenta);
+            crear_cuenta.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(view.getContext(), Registration.class);
+                    startActivityForResult(intent,0);
+                }
+            });
+
+
     }
 }
