caf13053beca2a9c4bd39e24b01123517d127ddb
CassandraHZ <106196257+CassandraHZ@users.noreply.github.com>
2022-05-30 18:27:43 -0500

    Added gesture

--- app/src/main/java/mx/ita/ayudadita/MainActivity.java
+++ app/src/main/java/mx/ita/ayudadita/MainActivity.java
@@ -2,19 +2,26 @@ package mx.ita.ayudadita;
 
 import android.content.Intent;
 import android.content.pm.LabeledIntent;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
-
+import android.gesture.Gesture;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 import mx.ita.ayudadita.databinding.ActivityMainBinding;
 
@@ -23,11 +30,11 @@ import mx.ita.ayudadita.databinding.ActivityMainBinding;
 //hola soy la modificacion de fer
 //hola soy commit fer
 //fer
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements GestureOverlayView.OnGesturePerformedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-
+    private GestureLibrary libreria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,14 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        libreria= GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if(!libreria.load()){
+            finish();
+        }
+        GestureOverlayView gestureView = findViewById(R.id.gestures);
+        gestureView.addOnGesturePerformedListener(this);
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -71,5 +86,19 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onGesturePerformed(GestureOverlayView gestureOverlayView, Gesture gesture) {
+        ArrayList<Prediction> predictions=libreria.recognize(gesture);
+
+        if(predictions.size()>0){
+            String comando = predictions.get(0).name;
+            if(comando.equals("ayuda")){
+                Intent settings = new Intent(this, fragment_about_us.class);
+                settings.putExtra("key", "Settings from gesture");
+                this.startActivity(settings);
+            }
+        }
     }
 }
