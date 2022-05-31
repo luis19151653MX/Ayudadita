da67407529a4bbff4f2f3f595404ffd299d09646
Fernando <luisgutierrez5-1@dgb.email>
2022-05-31 12:53:53 -0500

    fragmento semetres a preguntas

--- app/src/main/java/mx/ita/ayudadita/fragment_allquestions.java
+++ app/src/main/java/mx/ita/ayudadita/fragment_allquestions.java
@@ -3,19 +3,17 @@ package mx.ita.ayudadita;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_first_semester#newInstance} factory method to
+ * Use the {@link fragment_allquestions#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_first_semester extends Fragment {
+public class fragment_allquestions extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +24,7 @@ public class fragment_first_semester extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_first_semester() {
+    public fragment_allquestions() {
         // Required empty public constructor
     }
 
@@ -36,11 +34,11 @@ public class fragment_first_semester extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_first_semester.
+     * @return A new instance of fragment fragment_allquestions.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_first_semester newInstance(String param1, String param2) {
-        fragment_first_semester fragment = new fragment_first_semester();
+    public static fragment_allquestions newInstance(String param1, String param2) {
+        fragment_allquestions fragment = new fragment_allquestions();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,15 +59,6 @@ public class fragment_first_semester extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_first_semester, container, false);
-
-        ImageButton materia=view.findViewById(R.id.imageButton);
-        materia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_fragment_first_semester_to_fragment_allquestions);
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_allquestions, container, false);
     }
 }
