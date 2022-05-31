c30ebce97234135730894697a762d7dcef59981c
Fernando <luisgutierrez5-1@dgb.email>
2022-05-31 14:40:22 -0500

    adios semstres

--- app/src/main/java/mx/ita/ayudadita/fragment_write_question.java
+++ app/src/main/java/mx/ita/ayudadita/fragment_write_question.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link fragment_five_semester#newInstance} factory method to
+ * Use the {@link fragment_write_question#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class fragment_five_semester extends Fragment {
+public class fragment_write_question extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class fragment_five_semester extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public fragment_five_semester() {
+    public fragment_write_question() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class fragment_five_semester extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_five_semester.
+     * @return A new instance of fragment fragment_write_question.
      */
     // TODO: Rename and change types and number of parameters
-    public static fragment_five_semester newInstance(String param1, String param2) {
-        fragment_five_semester fragment = new fragment_five_semester();
+    public static fragment_write_question newInstance(String param1, String param2) {
+        fragment_write_question fragment = new fragment_write_question();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class fragment_five_semester extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_five_semester, container, false);
+        return inflater.inflate(R.layout.fragment_write_question, container, false);
     }
 }
