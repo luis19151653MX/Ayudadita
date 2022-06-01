e74cec1b46e0d744d371c511743a5f890eea41b4
Fernando <luisgutierrez5-1@dgb.email>
2022-06-01 01:16:34 -0500

    Comienzo idiomas, cambio a botones

--- app/src/main/java/mx/ita/ayudadita/ui/gallery/GalleryFragment.java
+++ app/src/main/java/mx/ita/ayudadita/ui/gallery/GalleryFragment.java
@@ -1,15 +1,20 @@
 package mx.ita.ayudadita.ui.gallery;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import java.util.Locale;
 
 import mx.ita.ayudadita.R;
 import mx.ita.ayudadita.databinding.FragmentGalleryBinding;
@@ -17,7 +22,7 @@ import mx.ita.ayudadita.databinding.FragmentGalleryBinding;
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
-    private RadioButton rbmode;
+    private Button buttonEs;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,10 +32,34 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //root.findViewById(R.id.radioButtonEs)
+        buttonEs=root.findViewById(R.id.buttonES);
+        buttonEs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         return root;
     }
+    /*
+    public void setLocal(String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("settings", MODE_PRIVATE).edit();
+        editor.putString("My_Language", lang);
+        editor.commit();
+    }
+
+    public void getLocal() {
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", Activity.MODE_PRIVATE);
+        String language = sharedPreferences.getString("My_Language", "");
+        setLocal(language);
+    }
+    */
 
     @Override
     public void onDestroyView() {
